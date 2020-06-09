5c8d6cc262ac6f04f5d636af1f5898d3010b00f1
Breg,Coen C.J <c.breg@student.fontys.nl>
2020-06-09 09:22:23 +0200

    changed router index

--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,10 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: () => import("../views/Day/Index")
-  },
-  {
     path: "/about",
     name: "about",
-    component: () => import("../views/About.vue")
+    component: () => import("../views/About.vue"),
+    meta: { requiresAuth: true }
   },
   {
     path: "/login",
@@ -22,32 +18,45 @@ const routes = [
   {
     path: "/day",
     name: "day.index",
-    component: () => import("../views/Day/Index")
+    component: () => import("../views/Day/Index"),
+    meta: { requiresAuth: true }
   },
   {
     path: "/day/create",
     name: "day.create",
-    component: () => import("../views/Day/Create")
+    component: () => import("../views/Day/Create"),
+    meta: { requiresAuth: true }
   },
   {
     path: "/day/sleep",
     name: "day.sleep",
-    component: () => import("../views/Day/Add")
+    component: () => import("../views/Day/Add"),
+    meta: { requiresAuth: true }
   },
   {
     path: "/day/edit",
     name: "day.edit",
-    component: () => import("../views/Day/Edit")
+    component: () => import("../views/Day/Edit"),
+    meta: { requiresAuth: true }
   },
   {
     path: "/activity/create",
     name: "activity.create",
-    component: () => import("../views/Activity/Create")
+    component: () => import("../views/Activity/Create"),
+    meta: { requiresAuth: true }
   }
 ];
 
 const router = new VueRouter({
   routes
 });
+
+router.beforeEach((to, from, next) => {
+  const loggedIn = localStorage.getItem('user')
+
+  if (to.matched.some(record => record.meta.requiresAuth) && !loggedIn)
+    next('/login')
+  else next()
+})
 
 export default router;
